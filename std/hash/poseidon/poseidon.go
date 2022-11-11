12295a93f7064cad5ec9fea12753af0b70f288a3
ZhAnGeek <lecky.z@nodereal.io>
2022-11-11 17:23:19 +0800

    fix: change constants to variable when meet

--- std/hash/poseidon/poseidon.go
+++ std/hash/poseidon/poseidon.go
@@ -1,6 +1,7 @@
 package poseidon
 
 import (
+	"github.com/consensys/gnark/backend/hint"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/compiled"
 	"github.com/consensys/gnark/std/hash/poseidon/constants"
@@ -73,7 +74,32 @@ func permutation(api frontend.API, state []frontend.Variable) []frontend.Variabl
 	return state
 }
 
+func preHandleData(api frontend.API, data ...frontend.Variable) []frontend.Variable {
+	// if all constants, skipped
+	constantCount := 0
+	for i := range data {
+		if _, isConstant := api.Compiler().ConstantValue(data[i]); isConstant {
+			constantCount++
+		}
+	}
+	if constantCount == len(data) {
+		return data
+	}
+
+	// get the self variables by hint, and make sure it is equal to data[i]
+	for i := range data {
+		self, err := api.Compiler().NewHint(hint.Self, 1, data[i])
+		if err != nil {
+			panic(err)
+		}
+		api.AssertIsEqual(self[0], data[i])
+		data[i] = self[0]
+	}
+	return data
+}
+
 func Poseidon(api frontend.API, data ...frontend.Variable) frontend.Variable {
+	data = preHandleData(api, data...)
 	// we record 2 params first, then lets see if we can record multi params and solve
 	t := len(data) + 1
 	if t < 3 || t > 13 {
